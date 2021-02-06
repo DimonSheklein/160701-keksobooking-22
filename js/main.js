3455a076d48041ac5e85f4ed9a9118a96975d1fd
DimonSheklein <mr.sheklein@gmail.com>
2021-02-06 11:52:46 +0300

    module2-task1: fix npm ERR

--- js/main.js
+++ js/main.js
@@ -6,44 +6,44 @@ const getRandomInteger = (min, max) => {
   let maxFloor = Math.floor(max);
   if (minCeil < 0 || maxFloor < 0) {
     throw new Error('диапазон может быть только положительный, включая ноль!')
-  };
+  }
   if (isNaN(min) || isNaN(max)) {
     throw new Error('в качестве параметров может быть только Number!')
-  };
+  }
   if (max <= min) {
     min = max;
     max = min;
-  };
+  }
 
   return Math.floor( Math.random() * (maxFloor - minCeil + 1) ) + minCeil; //за основу взял с https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 };
 
 try {
-  getRandomInteger(min, max);
+  getRandomInteger();
 } catch (err) {
-alert('Писец программе. Батя, спасай!');
+  alert('Писец программе. Батя, спасай!');
 }
 
 const getCoordinate = (min, max, count) => {
   if (min < 0 || max < 0) {
     throw new Error('диапазон может быть только положительный, включая ноль!')
-  };
+  }
   if (max <= min) {
     [min, max] = [max, min]
-  };
+  }
   if (isNaN(min) || isNaN(max)) {
     throw new Error('в качестве параметров может быть только Number!')
-  };
-const rundomNumber = Math.random() * (max - min + 1) + min;
-let fixedNum = '1e' + count;
+  }
+  const rundomNumber = Math.random() * (max - min + 1) + min;
+  let fixedNum = '1e' + count;
 
-return Math.floor(rundomNumber * fixedNum) / fixedNum;
+  return Math.floor(rundomNumber * fixedNum) / fixedNum;
 }
 
 try {
-  getCoordinate(min, max);
+  getCoordinate();
 } catch (err) {
-alert(err);
+  alert(err);
 }
 
 // console.log(getCoordinate(10, 20, 5));
