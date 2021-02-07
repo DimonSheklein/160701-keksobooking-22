3eb133bc4b38d5cc060c4b1e1961c744199c07b0
DimonSheklein <mr.sheklein@gmail.com>
2021-02-07 10:29:43 +0300

    Исправляет функции, объединяет проверку на исключения

--- js/main.js
+++ js/main.js
@@ -1,29 +1,26 @@
 'use strict';
 
-
+// Функция, возвращающая случайное целое число из переданного диапазона включительно
 const getRandomInteger = (min, max) => {
   let minCeil = Math.ceil(min);
   let maxFloor = Math.floor(max);
-  if (minCeil < 0 || maxFloor < 0) {
+  if (min < 0 || max < 0) {
     throw new Error('диапазон может быть только положительный, включая ноль!')
+  }
+  if (max <= min) {
+    minCeil = Math.ceil(max);
+    maxFloor = Math.floor(min);
   }
   if (isNaN(min) || isNaN(max)) {
     throw new Error('в качестве параметров может быть только Number!')
-  }
-  if (max <= min) {
-    min = max;
-    max = min;
   }
 
   return Math.floor( Math.random() * (maxFloor - minCeil + 1) ) + minCeil; //за основу взял с https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 };
 
-try {
-  getRandomInteger();
-} catch (err) {
-  alert('Писец программе. Батя, спасай!');
-}
+// console.log(getRandomInteger(20, 10, 5));
 
+// Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
 const getCoordinate = (min, max, count) => {
   if (min < 0 || max < 0) {
     throw new Error('диапазон может быть только положительный, включая ноль!')
@@ -34,24 +31,18 @@ const getCoordinate = (min, max, count) => {
   if (isNaN(min) || isNaN(max)) {
     throw new Error('в качестве параметров может быть только Number!')
   }
-  const rundomNumber = Math.random() * (max - min + 1) + min;
+  const rundomNumber = Math.random() * (max - min + 1) + min; //Максимум и минимум включаются
   let fixedNum = '1e' + count;
 
   return Math.floor(rundomNumber * fixedNum) / fixedNum;
 }
 
+// console.log(getCoordinate(10, 20, 5));
+
+// Проверка исключений
 try {
+  getRandomInteger();
   getCoordinate();
 } catch (err) {
   alert(err);
 }
-
-// console.log(getCoordinate(10, 20, 5));
-
-// Можно было наверно сократить количество кода следующей записью, только что-то не работает:(((
-// const getCoordinate1 = (min, max, count) => {
-// const fixedNum = '1e' + count;
-// return getCoordinate1 = Math.floor(getRandomInteger(min, max) * fixedNum) / fixedNum;
-// }
-
-// console.log(getCoordinate1(10, 20, 5));
