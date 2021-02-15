275bd9682a6e120fa2fa42c1f31c3401018f2936
DimonSheklein <mr.sheklein@gmail.com>
2021-02-15 19:54:08 +0300

    Причёсывает код после задания №3

--- js/main.js
+++ js/main.js
@@ -1,48 +1,9 @@
 'use strict';
 
-// Функция, возвращающая случайное целое число из переданного диапазона включительно
-const getRandomInteger = (...args) => {
-  let [min, max] = args;
-  if (min < 0 || max < 0) {
-    throw new Error('диапазон может быть только положительный, включая ноль!')
-  }
-  if (max <= min) {
-    [min, max] = [max, min];
-  }
-  if (isNaN(min) || isNaN(max)) {
-    throw new Error('в качестве параметров может быть только Number!')
-  }
-  if (min === max) {
-    return min;
-  }
-
-  return Math.floor( Math.random() * (max - min + 1) ) + min; //за основу взял с https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-};
-
-// Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
-const getCoordinate = (min, max, precision) => {
-  if (precision < 0 || isNaN(precision)) {
-    throw new Error('передан некорректный аргумент в качестве параметра!')
-  }
-
-  const mediator = Math.pow(10, precision);
-
-  return getRandomInteger(min * mediator, max * mediator) / mediator;
-};
-
-// Проверка исключений
-try {
-  getRandomInteger();
-  getCoordinate();
-} catch (err) {
-  alert(err);
-}
-
-// Массивы
 const TYPE_HOUSING = ['palace', 'flat', 'house', 'bungalow'];
 const FEATURES_HOUSING = ['wifi','dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const ROOMS = [1, 2, 3, 100];
-const TIME_CHECK_IN_OUT = ['12:00', '13:00', '14:00'];
+const TIME_TYPE = ['12:00', '13:00', '14:00'];
 const PHOTOS_GALLARY = [
   'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
@@ -63,48 +24,108 @@ const DESCRIPTIONS = [
   'Ночные посидели у костра под КиШа',
 ];
 
+const Price = {
+  MIN: 0,
+  MAX: 1e6,
+};
+const AvatarId = {
+  MIN: 1,
+  MAX: 8,
+};
+const Guests = {
+  MIN: 1,
+  MAX: 200,
+};
+const Coordinate = {
+  X: {
+    MIN: 35.65000,
+    MAX: 35.70000,
+  },
+  Y: {
+    MIN: 139.70000,
+    MAX: 139.80000,
+  },
+};
+const CHARACTERS = 5;
+
+// Функция, возвращающая случайное целое число из переданного диапазона включительно
+const getRandomInteger = (...args) => {
+  let [min, max] = args;
+  if (min < 0 || max < 0) {
+    throw new Error('диапазон может быть только положительный, включая ноль!')
+  }
+  if (max <= min) {
+    [min, max] = [max, min];
+  }
+  if (isNaN(min) || isNaN(max)) {
+    throw new Error('в качестве параметров может быть только Number!')
+  }
+  if (min === max) {
+    return min;
+  }
+
+  return Math.floor( Math.random() * (max - min + 1) ) + min; //за основу взял с https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+};
+
+// Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
+const getRandomFloatNumber = (min, max, precision) => {
+  if (precision < 0 || isNaN(precision)) {
+    throw new Error('передан некорректный аргумент в качестве параметра!')
+  }
+
+  const mediator = Math.pow(10, precision);
+
+  return getRandomInteger(min * mediator, max * mediator) / mediator;
+};
+
+// Проверка исключений
+try {
+  getRandomInteger();
+  getRandomFloatNumber();
+} catch (err) {
+  alert(err);
+}
+
+// Получаем рандомный эллемент массива
 const getRandomElement = (array) => {
   const index = getRandomInteger(0, array.length - 1);
   return array[index];
 };
 
+// Получаем случайный массив методом фильтрации
 const getSomeArray = (array) => {
   return array.filter(() => {
     return getRandomInteger(0, 1);
   });
 };
 
-// создание объекта для массива
+// Созданием объект для массива
 const getObject = function() {
-  const minPrice = 0;
-  const maxPrice = 1e6;
-  const minGuests = 1;
-  const maxGuests = 200;
-  const minCoordinateX = 35.65000;
-  const maxCoordinateX = 35.70000;
-  const minCoordinateY = 139.70000;
-  const maxCoordinateY = 139.80000;
-  const characters = 5;
+  const registry = getRandomElement(TIME_TYPE);
+  const avatarId = getRandomInteger(AvatarId.MIN, AvatarId.MAX);
 
+  // Описание автора
   const author = {
-    avatar: 'img/avatars/user0' + getRandomInteger(1, 8) + '.png',
+    avatar: 'img/avatars/user0' + avatarId + '.png',
   };
 
+  // Местоположение в виде географических координат
   const location = {
-    x: getCoordinate(minCoordinateX, maxCoordinateX, characters),
-    y: getCoordinate(minCoordinateY, maxCoordinateY, characters),
+    x: getRandomFloatNumber(Coordinate.X.MIN, Coordinate.X.MAX, CHARACTERS),
+    y: getRandomFloatNumber(Coordinate.Y.MIN, Coordinate.Y.MAX, CHARACTERS),
   };
 
+  // Иинформация об объявлении
   const offer = {
     title: getRandomElement(TITLES),
     description: getRandomElement(DESCRIPTIONS),
     address: Object.values(location).join(', '),
-    price: getRandomInteger(minPrice, maxPrice),
+    price: getRandomInteger(Price.MIN, Price.MAX),
     type: getRandomElement(TYPE_HOUSING),
     rooms: getRandomElement(ROOMS),
-    guests: getRandomInteger(minGuests, maxGuests),
-    checkin: getRandomElement(TIME_CHECK_IN_OUT),
-    checkout: getRandomElement(TIME_CHECK_IN_OUT),
+    guests: getRandomInteger(Guests.MIN, Guests.MAX),
+    checkin: registry,
+    checkout: registry,
     features: getSomeArray(FEATURES_HOUSING),
     photos: getSomeArray(PHOTOS_GALLARY),
   };
@@ -112,7 +133,7 @@ const getObject = function() {
   return {author, offer, location};
 };
 
-// создание массива из 10 сгенерированных JS-объектов
+// Генерируем массив из 10 объектов
 const getArray = function() {
   let array = [];
 
